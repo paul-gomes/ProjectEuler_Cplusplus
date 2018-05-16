e3ecfff82685a62282df1e81e74216e81f3b17da
Anik Paul Gomes <gomesap29@gmail.com>
2018-05-16 16:53:54 -0500

    problem 2 solved

---
+++ Prob2_Even_Fibonacci_numbers/cmake-build-debug/CMakeFiles/Prob2_Even_Fibonacci_numbers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prob2_Even_Fibonacci_numbers.dir/main.cpp.obj"
+  "Prob2_Even_Fibonacci_numbers.pdb"
+  "Prob2_Even_Fibonacci_numbers.exe"
+  "Prob2_Even_Fibonacci_numbers.exe.manifest"
+  "libProb2_Even_Fibonacci_numbers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prob2_Even_Fibonacci_numbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

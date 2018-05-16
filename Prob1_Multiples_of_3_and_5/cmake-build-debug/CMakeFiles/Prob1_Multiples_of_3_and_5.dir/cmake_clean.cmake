f57856640d2ef6d30d771fec0f847e6e5352c795
Anik Paul Gomes <gomesap29@gmail.com>
2018-05-16 16:33:11 -0500

    problem 1 solved

---
+++ Prob1_Multiples_of_3_and_5/cmake-build-debug/CMakeFiles/Prob1_Multiples_of_3_and_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prob1_Multiples_of_3_and_5.dir/main.cpp.obj"
+  "Prob1_Multiples_of_3_and_5.pdb"
+  "Prob1_Multiples_of_3_and_5.exe"
+  "Prob1_Multiples_of_3_and_5.exe.manifest"
+  "libProb1_Multiples_of_3_and_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prob1_Multiples_of_3_and_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Prob1_Multiples_of_3_and_5/main.cpp
@@ -0,0 +1,24 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int sum, number;
+    sum = 0;
+    number = 1;
+    while (number < 1000){
+        if((number % 3 == 0) or (number % 5 ==0)){
+            sum += number;
+
+        }
+        number += 1;
+    }
+    cout << "sum of all the multiples of 3 or 5 below 1000: " << sum << endl;
+    return 0;
+
+}
